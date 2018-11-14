0d7a4cb5bb0b4c18df640ef00720bf8f43400761
ricofung <ricofungcn@icloud.com>
2018-11-14 14:49:32 +0800

    修正配置，添加缓存

--- chok-eureka-client/src/main/java/com/Application.java
+++ chok-eureka-client/src/main/java/com/Application.java
@@ -1,16 +1,35 @@
 package com;
 
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
-@SpringBootApplication
-public class Application
+@ComponentScan(basePackages={"chok.common","com"})
+//exclude表示自动配置时排除特定配置（使用CommonsMultipartFile实现多文件上传）
+@EnableAutoConfiguration(exclude = { MultipartAutoConfiguration.class })
+public class Application extends SpringBootServletInitializer
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(Application.class);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
 	}
 }
